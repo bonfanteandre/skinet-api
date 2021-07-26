f4d1286df8c86b33584807ced9e63f3cd51181b0
André Bonfante <andre@universovarejo.com.br>
2021-07-26 20:50:44 -0300

    novos projetos

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,11 +1,10 @@
-using API.Data;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
