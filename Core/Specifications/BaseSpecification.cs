421752f2cec331f30425046ebae75f600ddef648
André Bonfante <andre@universovarejo.com.br>
2021-08-03 16:57:38 -0300

    adicionando filtros por marca e tipo

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -6,6 +6,11 @@ namespace Core.Specifications
 {
     public class BaseSpecification<T> : ISpecification<T>
     {
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
         public BaseSpecification()
         {
         }
@@ -14,14 +19,19 @@ namespace Core.Specifications
         {
             Criteria = criteria;
         }
-
-        public Expression<Func<T, bool>> Criteria { get; }
-
-        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
-
         protected void AddInclude(Expression<Func<T, object>> include)
         {
             Includes.Add(include);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
         }
     }
 }
